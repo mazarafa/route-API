d01af9b3e29fd471447f59bd81027ca8f4f91a41
mazarafa <arruda.rafa.ra@gmail.com>
2020-09-19 23:12:29 -0400

    trying to connect to mongodb through the docker

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// starting application
 const app = express();
+
+// starting the database
+mongoose.connect('mongodb://localhost:27017/router-API',{useNewUrlParser:True});
+
+//first route
 app.get('/',(req,res) => {
 
     res.send("Olá Mundo!");

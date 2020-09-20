025f9e185aa2d6cb0f8b070a065b39bd6274c112
mazarafa <arruda.rafa.ra@gmail.com>
2020-09-20 11:42:53 -0400

    Adicionando CORS ...

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // starting application
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // starting the database
 mongoose.connect(
